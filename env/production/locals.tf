8887916a8a4abc50e8214a7931662c537722b369
Thomas Sibley <tsibley@fredhutch.org>
2024-06-06 15:14:51 -0700

    forecasts-ncov is conceptually-associated with the ncov pathogen
    Grant it access to ncov's data.
    
    Related-to: <https://github.com/nextstrain/private/issues/110>
    Alternative-to: <https://github.com/nextstrain/infra/pull/16>

--- env/production/locals.tf
+++ env/production/locals.tf
@@ -6,6 +6,26 @@ locals {
   # this more easily.  This will likely evolve in the future to better support
   # our needs.
   #   -trs, 20 May 2024
+  #
+  # The forecasts-ncov repo is conceptually-speaking maybe best thought of as a
+  # forecasts/ directory within the ncov repo, similar to how the ncov-ingest
+  # repo is best thought of as an ingest/ directory within the same.¹  Unlike
+  # ncov-ingest, however, forecasts-ncov publishes files under its own name,
+  # e.g. <s3://nextstrain-data/files/workflows/forecasts-ncov/>, instead of
+  # ncov's name.  This is why it appears both as its own "pathogen" and as a
+  # repo associated with the "ncov" pathogen.  This situation grants
+  # forecasts-ncov more access to ncov than is really necessary—it only really
+  # needs <s3://nextstrain-ncov-private/metadata.tsv.zst>—but ah well, the
+  # alternatives are more complicated.  This same conceptual-confusion of
+  # considering "forecasts-ncov" to be a pathogen means it gets access to write
+  # <s3://nextstrain-data/forecasts_ncov….json>, which is also unnecessary.
+  #
+  # We could choose to address this some day, if the pattern of data-generating
+  # repos downstream of pathogen repos proliferates, by conceptually separating
+  # the two things.
+  #   -trs, 6 June 2024
+  #
+  # ¹ <https://github.com/nextstrain/private/issues/110#issuecomment-2153402857>
 
   pathogen_repos = tomap({
     # pathogen name   = [repo name, …]
@@ -13,7 +33,7 @@ locals {
     "forecasts-ncov"  = ["forecasts-ncov"],
     "measles"         = ["measles"],
     "mpox"            = ["mpox"],
-    "ncov"            = ["ncov", "ncov-ingest"],
+    "ncov"            = ["ncov", "ncov-ingest", "forecasts-ncov"],
     "rsv"             = ["rsv"],
     "seasonal-flu"    = ["seasonal-flu"],
     "zika"            = ["zika"],
